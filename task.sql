27d4d50ae9be9d2c27c30448a539105805ed906a
Poghos <90136116+Poghos777@users.noreply.github.com>
2024-03-01 02:55:35 +0400

    Update task.sql

--- task.sql
+++ task.sql
@@ -60,13 +60,13 @@ CREATE TABLE `guest_reviews` (
 --
 
 INSERT INTO `guest_reviews` (`id`, `firstName`, `lastName`, `Email`, `message`, `datetime`) VALUES
-(1, 'Hamlet', 'Sargsyan', 'sargsyan@mail.ru', 'barev dzez ', '2021-09-23 13:39:29'),
-(2, 'Sargis', 'Yeghiazaryan', 'sargis@mail.ru', 'shat mersi ', '2021-09-23 15:13:20'),
-(3, 'Narek', 'Mnacakanyan', 'narek@mail.ru', 'mi hrashq axchnak ', '2021-09-23 15:16:06'),
+(1, 'Hamlet', 'Sargsyan', 'sargsyan@mail.ru', 'hello world ', '2021-09-23 13:39:29'),
+(2, 'Sargis', 'Yeghiazaryan', 'sargis@mail.ru', 'shat hello world ', '2021-09-23 15:13:20'),
+(3, 'Narek', 'Mnacakanyan', 'narek@mail.ru', ' hello world ', '2021-09-23 15:16:06'),
 (4, 'Narek', 'Mnacakanyan', 'narek@mail.ru', 'shat shnorhakalutyun kayqi patrastman hamar', '2021-09-23 15:17:02'),
 (5, 'Hakob', 'Ghazaryan', 'ghazaryan@mail.ru', 'hargeli usanoxner,cankanum enq texekacnel,vor aysor hetadzgvel en bolor dasery ', '2021-09-23 15:23:09'),
 (7, 'Sergey', 'Danielyan', 'danielyan@mail.ru', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis quaerat assumenda, aut nemo dicta vel at incidunt fugit enim consequatur distinctio, porro quasi veniam. Maiores illo dolores cum enim a. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit odio similique magnam dolorem? Amet rerum cum quas commodi totam porro aliquam ducimus, eum sit nobis, omnis deleniti, et sequi quae.\r\nRecusandae saepe nisi id tenetur, quia magnam voluptates exercitationem eaque enim dignissimos esse optio ea eveniet, quas commodi. At odio fuga fugiat repellat dicta, sed excepturi commodi magnam laborum ad.\r\nVel a quisquam enim nostrum repellendus iste excepturi sunt, ex. Itaque, voluptatem corrupti velit blanditiis hic sapiente temporibus dolorum, vel consectetur mollitia omnis necessitatibus harum quos iusto voluptate, est similique!\r\nNon nostrum, ea quaerat suscipit cupiditate consequatur repudiandae a, minus voluptate optio reprehenderit, repellendus dolores voluptates. Ratione eaque quisquam minima harum cupiditate aperiam fugiat optio, quo deserunt sed autem consequatur?\r\nBeatae rem incidunt explicabo nostrum magnam, est fugiat sit velit eligendi ipsum quisquam repellendus blanditiis, fuga vitae veniam amet iusto nobis saepe doloremque corporis perspiciatis assumenda odio expedita natus quasi. ', '2021-09-23 21:44:32'),
-(8, 'Hayk', 'Saxatelyan', 'saxatel@mail.ru', 'bari galust xarabax telekom', '2021-09-24 19:07:04');
+(8, 'Hayk', 'Saxatelyan', 'saxatel@mail.ru', 'hello world', '2021-09-24 19:07:04');
 
 --
 -- Индексы сохранённых таблиц
